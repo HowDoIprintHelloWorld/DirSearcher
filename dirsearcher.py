1bfd0ba5a98ccfd385a7316629c4cea993cadfdf
HowDoIprintHelloWorld <87938212+HowDoIprintHelloWorld@users.noreply.github.com>
2021-12-09 17:13:21 +0100

    Update dirsearcher.py

--- dirsearcher.py
+++ dirsearcher.py
@@ -2,19 +2,24 @@ import requests, pyfiglet, sys, time
 
 wordlist = ""
 
+# Produces Banner 
 def startup():
   pyfiglet.print_figlet("DirSearcher", "slant")
   print("Like gobuster, but just a little slower")
 
+# Parses arguments
 def getargs():
   timeinterval = 0.1
+  
+  # Gets necessary wordlist and url
   if "-w" in sys.argv and "-u" in sys.argv:
     wordlist = sys.argv[sys.argv.index("-w") + 1]
     url = sys.argv[sys.argv.index("-u") + 1]
   else:
-    print("Please suppl url (-u) and wordlist (-w)")
+    print("Please supply url (-u) and wordlist (-w)")
     sys.exit()
     
+  # Gets optional arguments
   if "-t" in sys.argv:
     timeinterval = int(sys.argv[sys.argv.index("-t") + 1])
   return wordlist, url, timeinterval
@@ -22,11 +27,13 @@ def getargs():
 
 def wrdlist(wordlist):
   l = []
+  
+  # Tries to get words from wordlist supplied
   try: 
     with open(wordlist, "r") as file:
       for line in file:
         l.append(line.rstrip())
-  except:
+  except FileNotFoundError:
     print("Error loading file. Please try again.")
     return None, False
     
@@ -36,7 +43,7 @@ def wrdlist(wordlist):
   return l, True
 
 
-
+# Brains of the operation. Sends requests and analyses response
 def request(l, url, tint):
   worked = []
   for i in l:
@@ -48,7 +55,7 @@ def request(l, url, tint):
       print(f"{url}/{i} doesn't exist")
     time.sleep(tint)
 
-
+# Heart of the operation. Starts all the functions
 if __name__ == "__main__":
   worked = False
   startup()
